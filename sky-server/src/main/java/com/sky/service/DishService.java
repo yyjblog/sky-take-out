4b1a271e95ec9c3cec899904fcb0ad8bc693fa7e
姚英杰 <3356024752@qq.com>
2024-03-15 14:58:59 +0800

    商品浏览功能业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -59,4 +59,10 @@ public interface DishService {
      */
     List<Dish> list(Long categoryId);
 
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
